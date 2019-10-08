c9f9a9bdb68f01299de6047b6f5c72d06ea517c6
Administrator <Administrator@DNC-01712111555>
2019-10-08 14:41:47 +0800

    1

---
+++ src/FootBallPlayer.java
@@ -0,0 +1,13 @@
+
+public class FootBallPlayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		程序运行的代码都要写在主方法main里	
+		System.out.println("武磊是一个好球员");
+		System.out.println("里皮是一个好教练");
+		System.out.println("中国球迷心很累，人人都是数学家");
+	}
+// 做一个student类，在该类主方法里打印输出三句话：
+//	认真背单词，好好敲代码，努力找到好工作
+}
---
+++ src/Three.java
@@ -0,0 +1,19 @@
+
+public class Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("认真背单词");
+ System.out.println("好好敲代码");
+ System.out.println("努力找到好工作");
+ System.out.println("**********东软跨境电商交易平台**********");
+ System.out.println("*				*");
+ System.out.println("* 1.制造商供应管理			*");
+ System.out.println("* 2.销售商品品牌管理			*");
+ System.out.println("* 3.商品详细信息管理			*");
+ System.out.println("* 4.商品类别管理			*");
+ System.out.println("*				*");
+ System.out.println("*********************************");
+	}
+
+}
